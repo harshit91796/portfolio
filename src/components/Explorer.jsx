aab4a1fc26360b5fb85066827645729b3b739deb
Harshit <rajputharshit48@gmail.com>
2023-12-17 17:36:58 +0530

    first commit

--- src/components/Explorer.jsx
+++ src/components/Explorer.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import Experience from './section/Experience'
 import Skills from './section/Skills'
 import Achievment from './section/Achievment'
+import {css,js,f,node,react,vac,mon,my,i,socket} from '../assets/index'
 
 const Explorer = () => {
 
@@ -15,7 +16,7 @@ const Explorer = () => {
     <Link className='aboutMe'  to="/Ex">experience</Link>
     <Link className='aboutMe'  to="/skills">Skills</Link>
     <Link className='aboutMe'  to = "/achievment">Project</Link>
-    <Link className='aboutMe' style={{display : 'flex' ,gap : '5px', textAlign : 'center',alignItems : 'center'}} to = "/myPic"><div style={{ width : '20px' , height : '20px' }}><img style={{width : '100%', height : '100%'}} src="public\icons8-image-file-240.png" alt="" /></div> myPhoto.jpg</Link>
+    <Link className='aboutMe' style={{display : 'flex' ,gap : '5px', textAlign : 'center',alignItems : 'center'}} to = "/myPic"><div style={{ width : '20px' , height : '20px' }}><img style={{width : '100%', height : '100%'}} src={i} alt="" /></div> myPhoto.jpg</Link>
      
      
      
